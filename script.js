0d3b6a9894f562087260bd0367cc4ac461e411cb
Anabene <165837536+Anabene@users.noreply.github.com>
2026-09-22 10:51:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ const perguntas = [
     enunciado:
       "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
     alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
+    afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+  },
+  {
+    texto: "Isso é maravilhoso!",
+    afirmacao: "Quis saber como usar IA no seu dia a dia."
+  }
   },
   {
     enunciado:
